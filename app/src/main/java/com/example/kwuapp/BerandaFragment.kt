50e437d0cd3a1f51a18cceb8fb7d9a3ed0c0ce46
naufalirfani <naufal.irfani3@gmail.com>
2020-10-11 20:25:11 +0700

    update

--- app/src/main/java/com/example/kwuapp/BerandaFragment.kt
+++ app/src/main/java/com/example/kwuapp/BerandaFragment.kt
@@ -1,11 +1,6 @@
 package com.example.kwuapp
 
-
-import android.app.ProgressDialog
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View

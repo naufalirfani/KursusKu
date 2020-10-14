91d9efe0c26b329f3602e06440b7ff910bf1f7db
naufalirfani <naufal.irfani3@gmail.com>
2020-10-14 10:47:38 +0700

    new commit

--- app/src/main/java/com/example/kwuapp/MainActivity.kt
+++ app/src/main/java/com/example/kwuapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kwuapp
 
 import android.app.ProgressDialog
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle

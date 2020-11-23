d0d8ab273af07cf6dc27f0c8d50dc7d7800a49c5
naufalirfani <naufal.irfani3@gmail.com>
2020-11-23 22:34:49 +0700

    new commit

--- app/src/main/java/com/example/kwuapp/TopUpActivity.kt
+++ app/src/main/java/com/example/kwuapp/TopUpActivity.kt
@@ -3,12 +3,20 @@ package com.example.kwuapp
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_keranjang.*
+import kotlinx.android.synthetic.main.activity_keranjang.actionbar
+import kotlinx.android.synthetic.main.activity_top_up.*
 
+@Suppress("DEPRECATION")
 class TopUpActivity : AppCompatActivity() {
 
+    private var isTransferShow: Boolean = false
+    private var isPulsaShow: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top_up)
@@ -22,5 +30,43 @@ class TopUpActivity : AppCompatActivity() {
         actionbar.setBackgroundColor(resources.getColor(R.color.colorPrimary))
         tvTitle.setTextColor(Color.parseColor("#FFFFFF"))
         btnBack.background = resources.getDrawable(R.drawable.ic_arrow_back_white_24dp)
+
+        cons_metodebayar.bringToFront()
+        cons_transferbank.visibility = View.GONE
+        iv_trasnfer_down.setOnClickListener {
+            if(isTransferShow){
+                iv_trasnfer_down.setImageResource(R.drawable.ic_keyboard_arrow_down_black_24dp)
+                val animHide: Animation = AnimationUtils.loadAnimation(this, R.anim.up)
+                cons_transferbank.startAnimation(animHide)
+                cons_transferbank.visibility = View.GONE
+                isTransferShow = false
+            }
+            else{
+                iv_trasnfer_down.setImageResource(R.drawable.ic_keyboard_arrow_up_black_24dp)
+                val animShow: Animation = AnimationUtils.loadAnimation(this, R.anim.down)
+                cons_transferbank.startAnimation(animShow)
+                cons_transferbank.visibility = View.VISIBLE
+                isTransferShow = true
+            }
+        }
+
+        cons_bayarpulsa.bringToFront()
+        cons_pulsadetail.visibility = View.GONE
+        iv_pulsa_down.setOnClickListener {
+            if(isPulsaShow){
+                iv_pulsa_down.setImageResource(R.drawable.ic_keyboard_arrow_down_black_24dp)
+                val animHide: Animation = AnimationUtils.loadAnimation(this, R.anim.up)
+                cons_pulsadetail.startAnimation(animHide)
+                cons_pulsadetail.visibility = View.GONE
+                isPulsaShow = false
+            }
+            else{
+                iv_pulsa_down.setImageResource(R.drawable.ic_keyboard_arrow_up_black_24dp)
+                val animShow: Animation = AnimationUtils.loadAnimation(this, R.anim.down)
+                cons_pulsadetail.startAnimation(animShow)
+                cons_pulsadetail.visibility = View.VISIBLE
+                isPulsaShow = true
+            }
+        }
     }
 }
